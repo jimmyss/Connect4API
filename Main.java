ca096095756f8a7989ff07e08d2b1a617b216e68
Bohan Liu <bohanliu@Bohans-MacBook-Pro-2.local>
2024-09-22 14:38:57 -0400

    draft

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 
 
 public class Main {
-    Player player1 = new Player();
-    Player player2 = new Player();
+    int mode = Scanner
+    switch mode
+        case 1:
+            Player player1 = new Player();
+            Player player2 = new Player();
+        case 2:
+            Player player1 = new Player();
+            Player player2 = new Player();
+        case 3:
+            
+    Connect4 game = new Connect4(player1, player2);
+    while game.isEnd()
 }
